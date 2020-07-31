29ae77bae98c4645b45f6f964548989652ba983e
khoaHyh <khoahuynh.toronto@gmail.com>
2020-07-31 16:41:32 -0400

    Updates

---
+++ precache-manifest.be6c126f61feb42a745f8e51c6878b5d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25a37a1f3665ad9ca0307181f00342bb",
+    "url": "/issinfo/index.html"
+  },
+  {
+    "revision": "8bfdb5b3cdd302a3d208",
+    "url": "/issinfo/static/css/2.7c35e809.chunk.css"
+  },
+  {
+    "revision": "cb13d7e9b790c376758f",
+    "url": "/issinfo/static/css/main.fbe42135.chunk.css"
+  },
+  {
+    "revision": "8bfdb5b3cdd302a3d208",
+    "url": "/issinfo/static/js/2.dd3bda34.chunk.js"
+  },
+  {
+    "revision": "da75538c52411804769b514107a743de",
+    "url": "/issinfo/static/js/2.dd3bda34.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb13d7e9b790c376758f",
+    "url": "/issinfo/static/js/main.404dc43f.chunk.js"
+  },
+  {
+    "revision": "25908e8cd272bd946f2f",
+    "url": "/issinfo/static/js/runtime-main.637ae129.js"
+  }
+]);
